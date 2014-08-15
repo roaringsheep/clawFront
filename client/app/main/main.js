825f1395e084d114092442e526baa4c75fae6e0c
Minseon Song <songminseonk@gmail.com>
2014-08-15 13:55:58 -0400

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('clawFrontApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
