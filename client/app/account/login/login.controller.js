fbd95a443a65dd7a794a39814a7feb7ee06d3c69
Minseon Song <songminseonk@gmail.com>
2014-08-20 10:57:02 -0400

    got rid of two buttons, modal 'works'

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,29 +1,32 @@
 'use strict';
 
 angular.module('clawFrontApp')
-  .controller('LoginCtrl', function ($scope, Auth, $location, $window) {
-    $scope.user = {};
-    $scope.errors = {};
+    .controller('LoginCtrl', function($scope, Auth, $location, $window, $timeout) {
+        $scope.user = {};
+        $scope.errors = {};
 
-    $scope.login = function(form) {
-      $scope.submitted = true;
+        $scope.login = function(form) {
+            $scope.submitted = true;
+            if (form.$valid) {
+                Auth.login({
+                    email: $scope.user.email,
+                    password: $scope.user.password
+                })
+                    .then(function() {
+                        // Logged in, redirect to home
+                        $('#login').modal('hide');
+                        $timeout(function() {
+                          $location.path('/queue.waitPage');
+                        },450)
+                        
+                    })
+                    .catch(function(err) {
+                        $scope.errors.other = err.message;
+                    });
+            }
+        };
 
-      if(form.$valid) {
-        Auth.login({
-          email: $scope.user.email,
-          password: $scope.user.password
-        })
-        .then( function() {
-          // Logged in, redirect to home
-          $location.path('/queue.waitPage');
-        })
-        .catch( function(err) {
-          $scope.errors.other = err.message;
-        });
-      }
-    };
-
-    $scope.loginOauth = function(provider) {
-      $window.location.href = '/auth/' + provider;
-    };
-  });
+        $scope.loginOauth = function(provider) {
+            $window.location.href = '/auth/' + provider;
+        };
+    });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -14,7 +14,7 @@ angular.module('clawFrontApp')
 
     $scope.logout = function() {
       Auth.logout();
-      $location.path('/login');
+      $location.path('/');
     };
 
     $scope.isActive = function(route) {
