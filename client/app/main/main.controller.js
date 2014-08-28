98ebe2119268562340193f4cfd01833b791a6c49
Adam262 <abarcan@gmail.com>
2014-08-28 15:47:24 -0400

    fixed login modal

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,7 +4,9 @@ angular.module('clawFrontApp')
   .controller('MainCtrl', function ($scope, $http, socket, queueFactory) {
 
     $scope.toggleVideo = function() {
-      $('#yo').toggleClass('videoUrl1','videoUrl2');
+      
+      $('#yo').toggleClass('videoUrl2');
+      $('#yo').toggleClass('videoUrl');
     }
 
   });
