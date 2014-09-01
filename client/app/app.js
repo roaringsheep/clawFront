510c06d19b5a6a5c9c77fa5c24d9a3a693d5d854
Adam262 <abarcan@gmail.com>
2014-09-01 19:51:25 -0400

    queue head time stamp persists but keeps reseting with polling

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,8 @@ angular.module('clawFrontApp', [
   'btford.socket-io',
   'ui.router',
   'ui.bootstrap',
-  'ngTouch'
+  'ngTouch',
+  'timer'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
--- client/app/queue/queue.controller.js
+++ client/app/queue/queue.controller.js
@@ -6,38 +6,66 @@ angular.module('clawFrontApp')
         //Get current user
         $scope.currentUser = Auth.getCurrentUser();
         $scope.isLoggedIn = Auth.isLoggedIn();
-        console.log("currentUser: ", $scope.currentUser);
+        //console.log("currentUser: ", $scope.currentUser);
         $scope.queue = queueFactory.getQueue();
+        // persist time entering head of queue
+
+        $scope.persistHeadByCurrentUser = function(player) {
+            return queueFactory.persistHeadByCurrentUser(player);
+        }
+
+        $scope.persistHead = function(player) {
+            return queueFactory.persistHead(player);
+        }
+
+        // $scope.checkHead = function (player) {
+            
+        // }
 
         //Get queue
         $rootScope.$watch('queue', function(newval, oldval) {
             $scope.queue = newval;
+
             if (typeof $rootScope.queue == "undefined") {
                 // run nothing
             } else {
                 $rootScope.eta = queueFactory.ETAtoPlay(newval, $scope.currentUser);
+                $rootScope.playerInQueue = queueFactory.getPlayerInQueue(newval, $scope.currentUser);
+                $scope.persistHeadByCurrentUser($scope.currentUser);
             }
+
+
 
         })
 
-        $scope.credits = 1;
-
-        $scope.changeCredit = function (num){
-          $scope.credits = $scope.credits + num;
-
-          if($scope.credits === 0){
-            $scope.credits = 1;
-          }
-        }
-
-        $scope.resetCredits = function (){
-            return $scope.credits = 1;
-        }
 
         $rootScope.$watch('eta', function(newval, oldval) {
             $scope.eta = newval;
             console.log("I updated $rootScope.eta", $rootScope.eta, "$scope.eta: ", $scope.eta)
+
         })
+
+        $rootScope.$watch('playerInQueue', function(newval, oldval) {
+            $scope.playerInQueue = newval;
+            console.log("I updated $rootScope.playerInQueue", $rootScope.playerInQueue, "$scope.playerInQueue: ", $scope.playerInQueue)
+        })
+
+        //logic for buying credits
+
+        $scope.credits = 1;
+
+        $scope.changeCredit = function(num) {
+            $scope.credits = $scope.credits + num;
+
+            if ($scope.credits === 0) {
+                $scope.credits = 1;
+            }
+        }
+
+        $scope.resetCredits = function() {
+            return $scope.credits = 1;
+        }
+
 
         //Poll queue
         $interval(function() {
@@ -45,12 +73,13 @@ angular.module('clawFrontApp')
             if (typeof temp != "undefined") {
                 $rootScope.queue = queueFactory.getQueue()
             }
-        }, 10000)
+        }, 30001)
+
 
         //Add player
         $scope.addPlayer = function(player) {
-            if(player.credits>0){
-                alert ('awesome. you just joined the queue!');
+            if (player.credits > 0) {
+                alert('awesome. you just joined the queue!');
                 queueFactory.addPlayer(player);
                 console.log("player", player)
             } else {
@@ -59,65 +88,41 @@ angular.module('clawFrontApp')
         };
 
         //play game with credits
-        $scope.playGame = function(player) {
-            queueFactory.playWithCredits(player);
+        $scope.playGame = function(queue, player) {
+            queueFactory.playWithCredits(queue, player);
 
         };
 
 
+
         //Remove player
         $scope.removeByQueueUserId = function(player) {
-            queueFactory.removeByQueueUserId(player).success(function() {
+            return queueFactory.removeByQueueUserId(player).success(function() {
                 $scope.inQueue = false;
                 $rootScope.eta = queueFactory.ETAtoPlay($scope.queue, $scope.currentUser);
             });
         };
 
         $scope.removeByUserId = function(player) {
-            queueFactory.removeByUserId(player).success(function() {
+            return queueFactory.removeByUserId(player).success(function() {
                 $scope.inQueue = false;
                 alert("You just left the queue");
                 $rootScope.eta = queueFactory.ETAtoPlay($scope.queue, $scope.currentUser);
             });
         };
 
-        $scope.isInQueue = function(){
-            var bool = '';
-            var temp = queueFactory.getQueue();
-            if (temp != undefined) {
-            bool = queueFactory.findPlayerInQueue($scope.queue, currentUser)
-            }
-            bool>-1? $scope.inQueue = true: $scope.inQueue = false;
-        }
 
-
-    // $scope.submitStripe = function() {
-    //   console.log('I got here');
-    //   // var user = Auth.getCurrentUser();
-    //   // user.credits++;
-    //   // queueFactory.updateUser(user);
-    //   // console.log("token", response);
-    //   // if (response.error) {throw response.error;}
-    //   //     else {
-    //   //       var stripeToken = response.id;
-    //   //       var userId = $scope.currentUser._id;
-    //   //       $http.post('/charge', stripeToken, userId).success(function(){
-    //   //         console.log("$http post token, currentUser", stripeToken, userId)
-    //   //       });
-    //   //     }
-    // }
-
-        $scope.countdown = function() {
-            $interval(function() {
-                if ($scope.timer.countdown > 0) {
-                    $scope.timer.countdown--;
-                    console.log($scope.timer.countdown)
-                    if ($scope.timer.countdown == 0) {
-                        console.log("You're logged out!")
-                    }
-                }
-            }, 1000);
-        }
+        // $scope.countdown = function() {
+        //     $interval(function() {
+        //         if ($scope.timer.countdown > 0) {
+        //             $scope.timer.countdown--;
+        //             console.log($scope.timer.countdown)
+        //             if ($scope.timer.countdown == 0) {
+        //                 console.log("You're logged out!")
+        //             }
+        //         }
+        //     }, 1000);
+        // }
 
 
     });
