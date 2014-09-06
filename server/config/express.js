32884db20888f1e99adde9bb6061bb657819f0d1
Adam262 <abarcan@gmail.com>
2014-09-06 18:33:37 -0400

    yo

--- server/config/express.js
+++ server/config/express.js
@@ -18,7 +18,7 @@ var passport = require('passport');
 
 module.exports = function(app) {
   var env = app.get('env');
-  console.log(" node env: ", process.env.NODE_ENV)
+  console.log(" node env: ", env, process.env.NODE_ENV)
 
   app.set('views', config.root + '/server/views');
   app.engine('html', require('ejs').renderFile);
